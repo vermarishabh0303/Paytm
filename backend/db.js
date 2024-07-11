1e0d848efc9a7381c33752deaff886a151b1c8b1
Rishabh Verma <verma.rishabh0303@gmail.com>
2024-07-12 00:20:03 +0530

    backend completed

--- backend/db.js
+++ backend/db.js
@@ -30,12 +30,25 @@ const userSchema = mongoose.Schema({
         miniLength: 50
     }
     
-})
+});
 
+const accountSchema = mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance:{
+        type: Number,
+        required: true
+    }
 
+});
 
-const User = mongoose.model("User", userSchema);
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = {
-    User
+    User,
+    Account,
 };
