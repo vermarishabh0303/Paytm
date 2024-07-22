742e13838e9bd704d544e0032a1e5a3a13245087
Rishabh Verma <verma.rishabh0303@gmail.com>
2024-07-22 19:33:35 +0530

    UI made

--- frontend/src/pages/Singup.jsx
+++ frontend/src/pages/Singup.jsx
@@ -9,35 +9,35 @@ import axios from "axios"
 
 export const Signup = () => {
 
-    const[firstname, setFirstname] = useState("")
-    const[lastname, setLastname] = useState("")
-    const[email, setEmail] = useState("")
+    const[firstName, setFirstName] = useState("")
+    const[lastName, setLastName] = useState("")
+    const[password, setPassword] = useState("")
     const[username, setUsername] = useState("")
     const navigate = useNavigate()
-
+    
     return <div className="bg-indigo-950 h-screen flex justify-center">
         <div className="flex flex-col justify-center"> 
             <div className="rounded-lg bg-indigo-100 w-100 text-center p-2 h-max px-4">
                 <Heading label = {"Sign Up"}/>
                 <SubHeading label = {"Enter your information to create an account"}/>
-                <InputBox onChange={(e)=>{
-                    setFirstname(e.target.value)
-                }} placeholder="Enter your First Name" label={"First Name"} />
+                <InputBox onChange={e => {
+          setFirstName(e.target.value);
+        }} placeholder="John" label={"First Name"} />
                 <InputBox
                 onChange={(e)=>{
-                    setLastname(e.target.value)
+                    setLastName(e.target.value);
                 }} placeholder="Enter your Last Name" label={"Last Name"} />
                 <InputBox onChange={(e)=>{
-                    setEmail(e.target.value)
+                    setUsername(e.target.value);
                 }} placeholder={"Enter email"} label = {"Email"}/>
                 <InputBox onChange={(e)=>{
-                    setUsername(e.target.value)
+                    setPassword(e.target.value);
                 }} placeholder={"Enter Password"} label = {"Password"}/>
                 <div className="pt-4">
                     <Button onClick={async()=>{
                         const response = await axios.post("http://localhost:3001/api/v1/user/signup",{
-                            firstname,
-                            lastname,
+                            firstName,
+                            lastName,
                             username,
                             password        
                         });
